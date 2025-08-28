cc8204a92343dda5c751d202b7b453971874abf6
SQA Manuel <manuel.buelvas@sqasa.co>
2025-08-28 11:59:28 -0500

    commit2 para el reto

---
+++ src/OtrasVariabes.java
@@ -0,0 +1,9 @@
+public class OtrasVariabes {
+    public static void main(String[] args) {
+        double sueldo = 2000.10;
+        boolean soltero = true;
+
+        System.out.println("Mi sueldo es: " + sueldo);
+        System.out.println("Soy soltero: " + soltero);
+    }
+}
