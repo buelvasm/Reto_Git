a72428d05dade605cd6fa4067a8e2bf41e6ec185
SQA Manuel <manuel.buelvas@sqasa.co>
2025-08-28 11:46:52 -0500

    commit1 para el reto

---
+++ src/ClaseOne.java
@@ -0,0 +1,5 @@
+public class ClaseOne {
+    public static void main(String[] args) {
+        System.out.println("Hola Java");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int Edad = 29;
+        String Nombre;
+        Nombre= "Manuel";
+
+        System.out.println("Mi nombre es: " + Nombre + " Y tengo " +Edad + " años");
+    }
+}
