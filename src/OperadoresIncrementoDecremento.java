865180129df2af08f3384d55a6df591e69cbe21b
buelvasm <buelvasm@hotmail.es>
2025-09-02 06:13:33 -0500

    Se agrega clase de Operadores de incremento y decremento

---
+++ src/OperadoresIncrementoDecremento.java
@@ -0,0 +1,14 @@
+public class OperadoresIncrementoDecremento {
+    public static void main(String[] args) {
+        int numeroUno, numeroDos;
+        numeroUno = 0;
+        numeroDos = 5;
+
+        //numeroUno = numeroUno + 1;
+        numeroUno++;
+        numeroDos--;
+
+        System.out.println("El incremento es:" + numeroUno);
+        System.out.println("El incremento es:" + numeroDos);
+    }
+}
